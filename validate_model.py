fix #8: implement MAP@n metrics and complete cross-validation with scores computation

validate_model.py
@@ -1,24 +1,36 @@
-from tools import create_submission, load_data
-from models import FrequencyModel
-from validation import KFoldCrossValidation
+import numpy as np
+from tools import load_data
+from models import FrequencyModel, TfIdfModel
+from validation import KFoldCrossValidation, MAPn
 
 # load data
 path_to_data = "../data/"
 training, training_info, test, test_info = load_data(path_to_data)
 
 # create model
-frequency_model = FrequencyModel(nb_recipients_to_predict=10)
+nb_recipients_to_predict = 10
+model = FrequencyModel(nb_recipients_to_predict=nb_recipients_to_predict)
 
 # validation
 n_split = 5
 cross_validation = KFoldCrossValidation(n_split)
 
+scores = []
 for fold_nb, (training_fold, training_info_fold, test_fold, test_info_fold, y_test_fold) in enumerate(
         cross_validation.split(training, training_info)):
     print("\nFold #%d..." % fold_nb)
 
     # fit model
-    frequency_model.fit(training_fold, training_info_fold)
+    model.fit(training_fold, training_info_fold)
 
     # predict
-    frequency_model.predict(test_fold, test_info_fold)
+    predictions_per_sender = model.predict(test_fold, test_info_fold)
+
+    # compute MAP@n
+    score = MAPn(predictions_per_sender, y_test_fold, nb_recipients_to_predict)
+    scores.append(score)
+    print("\nMAP@n for this fold -> %0.5f" % score)
+
+# average scores over folds
+final_score = np.mean(scores)
+print("\n Final Score -> %0.5f" % final_score)

validation/kfold_cross_validation.py
@@ -24,10 +24,10 @@ class KFoldCrossValidation:
             max_percentage = percentages[fold_nb + 1]
 
             # retrieve mids for training and test datasets
-            training_mids = total_mids.apply(
+            test_mids = total_mids.apply(
                 lambda mids: mids[np.floor(min_percentage * len(mids)).astype(int): np.floor(
                     max_percentage * len(mids)).astype(int)])
-            test_mids = total_mids.apply(
+            training_mids = total_mids.apply(
                 lambda mids: np.concatenate([mids[:np.floor(min_percentage * len(mids)).astype(int)],
                                              mids[np.floor(max_percentage * len(mids)).astype(int):]]).tolist())
 
@@ -42,6 +42,10 @@ class KFoldCrossValidation:
             # create training_info_fold and test_info_fold, by slicing original training_info
             training_info_fold = training_info[training_info["mid"].isin(flatten_training_mids)]
             test_info_fold = training_info[training_info["mid"].isin(flatten_test_mids)]
+
+            # reset index of training_info_fold, test_info_fold
+            training_info_fold.reset_index(inplace=True, drop=True)
+            test_info_fold.reset_index(inplace=True, drop=True)
 
             # remove recipients from test_info_fold
             y_test_fold = pd.Series(test_info_fold["recipients"].values, index=test_info_fold["mid"])